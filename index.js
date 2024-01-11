45c17733d475d439dccb4b71a4fa6412aa82b784
hazu <100129304+Pricefilter@users.noreply.github.com>
2024-01-11 11:39:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ bot.on('message', async (ctx, next) => {
         await ctx.replyWithPhoto(chart,{caption: strMess, message_thread_id: threadID, reply_markup: {
           inline_keyboard: [
             /* Inline buttons. 2 side-by-side */
-            [ { text: "💯 Add Giỏ Live", url: "https://t.me/CoNenChotDon/1464" }],
+            [ { text: "💯 Add Live 💯", url: "https://t.me/CoNenChotDon/1464" }, { text: "💯 Add Video 💯", url: "https://t.me/CoNenChotDon/14113" }],
 
             /* One button */
             //[ { text: "❓Hướng Dẫn", url: "https://t.me/ChotDonBot" }, { text: "🔥 15 Voucher 50K", url: "https://www.facebook.com/groups/salelameofficial/"}]
