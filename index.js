5288fa2cb6600414593220f6b5689f2e3b84892c
hazu <100129304+Pricefilter@users.noreply.github.com>
2024-04-17 15:13:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -260,7 +260,7 @@ bot.on('message', async (ctx, next) => {
       const maxRetries = 5;
       while (retryCount < maxRetries) {
         try {
-        const respee = await fetch(url.replace(/https:\/\/vn\./, "https://"))
+        const respee = await fetch(${url.replace(/https:\/\/vn\./, "https://")})
         const resURL = await decodeURIComponent(respee.url.replace(/https:\/\/shopee\.vn\/universal-link\?af=false&deep_and_deferred=1&redir=/gm,''))
         const peeDlink = resURL.match(/(.*?)\?/)[1]
         console.log(peeDlink)
